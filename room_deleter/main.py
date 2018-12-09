6e78a075f0b7a3447bac246080c20eea869d9c01
Mike Wilkinson <mwilkinson@imo-online.com>
2018-12-08 18:06:41 -0600

    add room_delete lambda

---
+++ room_deleter/main.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+from config import Config
+from boto3.dynamodb.conditions import Key, Attr
+
+
+def handler(event, context):
+    print(event)
+    client_id = event['clientId']
+    
+    #get the client's room
+    dynamodb = boto3.resource('dynamodb', region_name=Config.AWS_REGION)
+    table = dynamodb.Table(Config.ROOM_TABLE)
+    response = table.scan(
+        FilterExpression=Attr('host').eq(client_id)
+    )
+    data = response['Items']
+    if len(data) == 1: #delete the room
+        response = table.delete_item(
+            Key={
+                'roomName': data[0]['roomName']
+            }
+        )
+        print('Closing room: ' + data[0]['roomName'])
+        print(response)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Ok')
+    }
